f189be890725600dffe48002f6610d533dda7f59
Fred Neumann <fred.neumann@fau.de>
2022-02-14 23:53:25 +0100

    Fixed player for ILIAS 7

--- class.ilInteractiveVideoFauVideoPortalGUI.php
+++ class.ilInteractiveVideoFauVideoPortalGUI.php
@@ -56,9 +56,7 @@ class ilInteractiveVideoFauVideoPortalGUI implements ilInteractiveVideoSourceGUI
 	public function addPlayerElements($tpl)
 	{
 		$tpl->addJavaScript('Customizing/global/plugins/Services/Repository/RepositoryObject/InteractiveVideo/VideoSources/plugin/InteractiveVideoFauVideoPortal/js/jquery.InteractiveVideoFauVideoPortalPlayer.js');
-        // fau: jumpMedia - prevent custom player for interactive video
-        ilPlayerUtil::initMediaElementJs($tpl, false);
-        // fau.
+        ilPlayerUtil::initMediaElementJs($tpl);
 		return $tpl;
 	}
 
